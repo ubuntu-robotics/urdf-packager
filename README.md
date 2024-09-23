d7860afefccc64eb97359c89406d42978af85492
artivis <jeremie.deray@canonical.com>
2024-09-23 17:49:07 +0200

    init
    Signed-off-by: artivis <jeremie.deray@canonical.com>

---
+++ README.md
@@ -0,0 +1,56 @@
+# urdf-packager
+
+A small util to turn a robot model into a single self-contained archive.
+
+## Use
+
+### Env setup
+
+Create a ROS 2 ready environment:
+
+```bash
+lxc launch ubuntu:22.04 urdf-exporter --config=user.user-data="$(curl -L https://gist.githubusercontent.com/artivis/1fcfcc38f1cb8c087e27ebda5484f2ef/raw/ros-bare.cloud-init.yaml)"
+```
+
+Shell inside the environment as the `ubuntu` user:
+
+```bash
+lxc exec urdf-exporter --mode interactive -- /bin/sh -xac $@ubuntu - exec /bin/login -p -f
+```
+
+Create a workspace and clone the project:
+
+```bash
+mkdir -p ~/workspace/src
+cd ~/workspace/src
+git clone https://github.com/ros-navigation/nav2_minimal_turtlebot_simulation.git
+```
+
+Install the dependencies:
+
+```bash
+source /opt/ros/*/setup.bash
+rosdep install --from-path . -y -i
+```
+
+### Convert XACRO to SDF
+
+```bash
+xacro -o output.sdf path/to/input.sdf.xacro
+```
+
+```bash
+xacro -o /tmp/gz_waffle.sdf ~/workspace/src/nav2_minimal_turtlebot_simulation/nav2_minimal_tb3_sim/urdf/gz_waffle.sdf.xacro
+```
+
+### Make it self-contained
+
+```bash
+python3 main.py path/to/output.sdf -p path/to/original/package/source
+```
+
+```bash
+python3 main.py /tmp/gz_waffle.sdf -p ~/workspace/src/nav2_minimal_turtlebot_simulation
+```
+
+This creates the `model.tar.gz` archive in the `./` folder.
---
+++ main.py
@@ -0,0 +1,83 @@
+import argparse
+import copy
+import os
+import shutil
+import sys
+import tarfile
+import tempfile
+import xml.etree.ElementTree as ET
+from pathlib import Path
+
+import resolve_robotics_uri_py
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Load a file from a given path.")
+    parser.add_argument("file", help="The name of the file to load.")
+    parser.add_argument(
+        "-o", "--output-dir", default=".", help="The path where to save the archive."
+    )
+    parser.add_argument(
+        '-p',
+        "--package_dirs",
+        default=None,
+        nargs="+",
+        help="The paths where the XML files are located. If not provided, uses the current working directory."
+    )
+
+    return parser.parse_args()
+
+def check_file_exists(file_path):
+    if not os.path.isfile(file_path):
+        print(f"Error: '{file_path}' does not exist.")
+        sys.exit(1)
+
+def load_sdf_file(file_path):
+    try:
+        tree = ET.parse(file_path)
+        return tree
+    except ET.ParseError:
+        print(f"Error parsing XML file '{file_path}': Invalid XML")
+        sys.exit(1)
+    except Exception as e:
+        print(f"Error loading XML file '{file_path}': {e}")
+        sys.exit(1)
+
+def find_and_resolve(tree, tag_name, package_dirs = None):
+    update = copy.deepcopy(tree)
+    files = []
+    for elem in update.iter():
+        if elem.tag == tag_name:
+            uri = elem.text
+            absolute_path = resolve_robotics_uri_py.resolve_robotics_uri(uri, package_dirs)
+            elem.text = './' + os.path.basename(absolute_path)
+            files.append(absolute_path)
+    return update, files
+
+def main(args):
+    check_file_exists(args.file)
+
+    with tempfile.TemporaryDirectory() as tmp_dir_str:
+        tmp_dir = Path(tmp_dir_str)
+
+        file_copy = tmp_dir / os.path.basename(args.file)
+
+        content = load_sdf_file(args.file)
+        content, files = find_and_resolve(content, 'uri', args.package_dirs)
+
+        content.write(file_copy, encoding="utf-8")
+
+        # Copy files to the temporary directory
+        for f in files:
+            shutil.copy2(f, tmp_dir / os.path.basename(f))
+
+        # Create tar.gz archive from the temporary directory
+        archive_path = Path(args.output_dir) / "model.tar.gz"
+
+        with tarfile.open(archive_path, "w:gz") as tarball:
+            tarball.add(tmp_dir.resolve(), arcname='model')
+
+
+if __name__ == "__main__":
+    args = parse_arguments()
+    main(args)
